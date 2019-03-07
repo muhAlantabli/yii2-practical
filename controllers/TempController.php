0b87463a4a9a8ece130ee267127466e47620a79b
Muhammad Alantabli <muhalantabli@gmail.com>
2019-03-07 03:39:40 +0200

    use base controller

---
+++ controllers/TempController.php
@@ -0,0 +1,44 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: muhAlantabli
+ * Date: 3/7/19
+ * Time: 12:37 AM
+ */
+
+namespace app\controllers;
+
+
+use app\components\BaseController;
+use yii\helpers\ArrayHelper;
+
+class TempController extends BaseController
+{
+    public function actions()
+    {
+        return ArrayHelper::merge(parent::actions(), [
+           'page' => ['class' => 'yii\web\ViewAction', 'defaultView' => 'page']
+        ]);
+    }
+
+    public function behaviors()
+    {
+        $behaviors = parent::behaviors();
+        $rules = $behaviors['access']['rules'];
+        $rules = ArrayHelper::merge($rules,[
+            [
+                'allow' => true,
+                'actions' => ['page'],
+            ],
+        ]);
+
+        $behaviors['access']['rules'] = $rules;
+        return $behaviors;
+    }
+
+    public function actionIndex()
+    {
+        return $this->renderContent('index');
+    }
+
+}
